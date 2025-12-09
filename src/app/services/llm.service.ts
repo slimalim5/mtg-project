5c149127db95e0b69f04f0a1cb62dbc233d03b0f
will-elsen <willtelsen@gmail.com>
2025-12-09 16:58:13 -0500

    started chat stuff

--- src/app/services/llm.service.ts
+++ src/app/services/llm.service.ts
@@ -2,18 +2,16 @@ import { inject, Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { OpenAI } from 'openai';
 import { environment } from '../../environments/environment';
+import { SecretCardData } from '../models/game.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LlmService {
   
-  private client = new OpenAI({
-    apiKey: environment.openai.apiKey,
-    // This is dangerous, and were this going to be a production app, this would need to be false, and the ai call would need to happen in Firebase
-    dangerouslyAllowBrowser: true,
-  })
-  http = inject(HttpClient);
+  answerQuestion(question: string, card: SecretCardData) {
+    
+  }
 
   constructor() { }
 }
